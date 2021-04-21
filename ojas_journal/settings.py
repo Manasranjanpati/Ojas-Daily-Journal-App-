1106e3f8ccd07d7100e1f8b0c25967a3e9ba0aa4
srustith8 <srustith8@gmail.com>
2021-04-21 19:04:20 +0530

    updated user registration and other files

--- ojas_journal/settings.py
+++ ojas_journal/settings.py
@@ -81,6 +81,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'journal',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+# }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -129,3 +139,10 @@ LOGIN_REDIRECT_URL = 'journal_app-home'
 LOGIN_URL = 'login'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'pythonpy109@gmail.com'
+EMAIL_HOST_PASSWORD = 'P@ss1234'
+EMAIL_PORT = 587
--- users/urls.py
+++ users/urls.py
@@ -2,15 +2,13 @@
 
 from django.contrib.auth import views as auth_views
 from django.urls import path
-
-from users import views as user_views
+from users import views as user_views,views
 
 urlpatterns = [
-
-    path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-
+    path('signup/', views.signup, name='register'),
+    path('activate/<slug:uidb64>/<slug:token>/', views.activate, name='activate'),
 ]
 
