99de55846f3201b1fdf4d21a1058867bc50a7582
Manasranjan Pati <mp21162@corp.ojas-it.com>
2021-04-22 16:28:16 +0530

    small design changes

--- ojas_journal/urls.py
+++ ojas_journal/urls.py
@@ -17,13 +17,11 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', include('journal_app.urls')),
-
 ]
 
 
