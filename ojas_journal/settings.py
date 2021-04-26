492879087085c34a8fbc24dbdd09d9087b81e670
mallika <mallika.sk@ojas-it.com>
2021-04-26 16:19:13 +0530

    .env is added

--- ojas_journal/settings.py
+++ ojas_journal/settings.py
@@ -7,7 +7,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv, find_dotenv
+load_dotenv()
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -17,7 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
+
+
+SECRET_KEY = os.environ.get("SECRETKEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
